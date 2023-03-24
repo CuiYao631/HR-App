d5eaf3b1841779820f427a369b55902cf74ac185
xiaocui <631836754@qq.com>
2023-03-24 11:25:18 +0800

    update

--- HR-App/MainWindow.xaml.cs
+++ HR-App/MainWindow.xaml.cs
@@ -1,28 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using AduSkin.Controls.Metro;
 
 namespace HR_App
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            StateChanged += (o, e) =>
+            {
+                if (WindowState == WindowState.Minimized)
+                {
+                    Hide();
+                }
+            };
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        System.Windows.Forms.NotifyIcon icon = null;
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            icon = new System.Windows.Forms.NotifyIcon();
+            icon.BalloonTipText = this.Title;
+            icon.Text = Title;
+            icon.Icon = new System.Drawing.Icon(@"icon.ico");
+            icon.Visible = true;
+            icon.Click += (o, e1) =>
+            {
+                this.Show();
+                WindowState = WindowState.Normal;
+            };
+
         }
     }
 }
